2d16e7fcfb4dd19b046451f4474ca0edb82fdd37
Robert Rondon <robrondon11@gmail.com>
2023-01-14 23:42:44 -0300

    simple backend

---
+++ controllers/user.controller.js
@@ -0,0 +1,46 @@
+const { response } = require('express');
+
+const getUsers = (req, res = response) => {
+  const { q, name = 'No name', apikey, page = 1, limit } = req.query;
+  res.json({
+    msg: 'get API - controller',
+    q,
+    name,
+    apikey,
+    page,
+    limit,
+  });
+};
+const postUsers = (req, res = response) => {
+  const { name, age } = req.body;
+  res.json({
+    msg: 'post API - controller',
+    name,
+    age,
+  });
+};
+const putUsers = (req, res = response) => {
+  const { id } = req.params;
+  res.json({
+    msg: 'put API - controller',
+    id,
+  });
+};
+const patchUsers = (req, res = response) => {
+  res.json({
+    msg: 'patch API - controller',
+  });
+};
+const deleteUsers = (req, res = response) => {
+  res.json({
+    msg: 'delete API - controller',
+  });
+};
+
+module.exports = {
+  getUsers,
+  postUsers,
+  putUsers,
+  patchUsers,
+  deleteUsers,
+};
---
+++ models/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usersPath = '/api/users';
+
+    // Middlewares
+    this.middlewares();
+
+    // Application Routes
+    this.routes();
+  }
+
+  routes() {
+    this.app.use(this.usersPath, require('../routes/user.routes.js'));
+  }
+
+  listen() {
+    this.app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${this.port}`)
+    );
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+
+    // Read and parse info
+    this.app.use(express.json());
+
+    // Public Directory
+    this.app.use(express.static('public'));
+  }
+}
+
+module.exports = Server;
